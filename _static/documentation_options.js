80693c82a4732807a802af033320c25e0555a34c
Stefan Arentz <stefan.arentz@aiven.io>
2022-03-31 21:14:04 +0000

    From refs/heads/main c92c8400

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.1-0-unknown-37ad1e2',
+    VERSION: '0.0.1-0-unknown-c92c840',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
